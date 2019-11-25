f18ea34cad38cd8cf8af542e5d2c3eb57c8e4421
Tri Pham <Tri.PhamMinh@mitrais.com>
2019-11-25 15:12:53 +0700

    update missing file

---
+++ src/main/java/atmsilmulation/exception/FundTransactionException.java
@@ -0,0 +1,7 @@
+package atmsilmulation.exception;
+
+public class FundTransactionException extends Exception {
+    public FundTransactionException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/atmsilmulation/exception/WithdrawException.java
@@ -0,0 +1,7 @@
+package atmsilmulation.exception;
+
+public class WithdrawException extends Exception {
+    public WithdrawException(String message) {
+        super(message);
+    }
+}
