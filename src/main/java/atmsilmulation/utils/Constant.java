007918a439d5949efed17766a90827041b708539
Tri Pham <Tri.PhamMinh@mitrais.com>
2019-12-09 16:16:36 +0700

    Resolve confilct

--- src/main/java/atmsilmulation/utils/Constant.java
+++ src/main/java/atmsilmulation/utils/Constant.java
@@ -1,9 +1,7 @@
 package atmsilmulation.utils;
 
 public class Constant {
-    public static final String SUCCESS = "SUCCESS";
-<<<<<<< HEAD
-=======
+
     public static final String ACCOUNT_INVALID = "Account is invalid";
     public static final String INSUFFICIENT_BALANCE = "Insufficient balance $";
     public static final String MAXIMUM_TRANSFER_AMOUNT = "Maximum amount to transfer is $1000";
@@ -11,5 +9,5 @@ public class Constant {
     public static final String INVALID_AMOUNT = "Invalid amount";
     public static final String MAXIMUM_WITHDRAW_AMOUNT = "Maximum amount to withdraw is $1000";
     public static final String CHOOSE_WITHDRAW_AMOUNT = "Please choose withdraw amount";
->>>>>>> f18ea34cad38cd8cf8af542e5d2c3eb57c8e4421
+
 }
